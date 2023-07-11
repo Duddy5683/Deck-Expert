8ad8245f5224c809466528ad6ad86ac2cdec1ebb
Duddy5683 <126365878+Duddy5683@users.noreply.github.com>
2023-07-11 06:40:02 +0000

    fixed

--- pages/index.js
+++ pages/index.js
@@ -74,9 +74,8 @@ const Home = () => {
             <h2>"Unlocking Tips, Techniques, and Expertise for Stunning Decks"</h2>
           </div>
         </div>
-          <div className="logo-container">
-            <Image src={deckexpertLogo} alt="Your logo" />
-        </div>
+        <div className="logo-container">
+          <Image src={deckexpertLogo} alt="Your logo" />
         </div>
         <div className="prompt-container">
           <textarea
@@ -110,6 +109,7 @@ const Home = () => {
             </div>
           )}
         </div>
+      </div>
       <div className="badge-container grow">
         <a href="https://buildspace.so/builds/ai-writer" target="_blank" rel="noreferrer">
           <div className="badge">
